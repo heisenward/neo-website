8104b63149f222bd3bb34f78ab6e3814e34d76cd
Bnjw Qjvxw <hamonsean202@gmail.com>
2024-04-16 19:50:55 -0500

    gambling

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function updateParallax() {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var parallaxFactor = -0.5; // Adjust this value to control the intensity of the parallax effect
     var backgroundOffset = scrollTop * parallaxFactor;
-    document.querySelector('.background').style.backgroundPositionY = backgroundOffset + 'px';
+    document.querySelector('.gamble-background', '.background').style.backgroundPositionY = backgroundOffset + 'px';
     
     requestAnimationFrame(updateParallax);
   }
