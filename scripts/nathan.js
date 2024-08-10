970aa407405dbb0390f64d90c81e8f77bda4c9f6
NAthan <nathanbsmith729@gmail.com>
2024-08-10 13:09:22 -0500

    ACTUAL FINAL UPDATE

--- scripts/nathan.js
+++ scripts/nathan.js
@@ -1,10 +1,1 @@
-// nathan.js
-
-function playAudio() {
-    var audio = document.getElementById('audio0');
-    if (audio) {
-        audio.play();
-    } else {
-        console.error('Audio element not found');
-    }
-}
+console.log('HHHHEEEYYYYY')
