e3cc1bfa69d0d66399b66f0e1740ef38f345ddc1
NAthan <nathanbsmith729@gmail.com>
2024-08-25 20:00:49 -0500

    data stealer 2.0

--- nathan.js
+++ nathan.js
@@ -1,33 +1,35 @@
 let SSN;
 document.getElementById("button").onclick = function()
 { SSN = document.getElementById("input").value;
-console.log(`SSN: ${SSN}`);
+console.log(`SSN: ${typeof SSN} ${SSN}`);
   window.alert("Thank you!")
   checkSSN();
-document.getElementById("KK").textContent = ("I willl be selling thiss!!")
 }
 function checkSSN(){
   if (typeof SSN === 'undefined' || SSN === ''){
       window.alert("heeyyyy, theres no data here!");
 
-    if (typeof SSN !== 'number' || isNaN(SSN)){
-      window.alert("it has to a number silly!");}
+     if (typeof SSN !== 'number' || isNaN(SSN) === true || typeof SNN === 'string'){
+      window.alert("And it has to a number");}
+      document.getElementById("KK").textContent = ("I willl be selling thiss!!")
   }
 }
 
   let CC;
   document.getElementById("button2").onclick = function(){
     CC = document.getElementById("input2").value; 
-    console.log(`Credit Card Number: ${CC}`);
+    console.log(`Credit Card Number: ${typeof CC} ${CC}`);
     window.alert("Thank you!")
     checkCC();
 }
 
-function checkCC(){
+function checkCC(){ 
   if (typeof CC === 'undefined' || CC === ''){
-    window.alert("heeyyyy, theres no data here!");
- if (typeof CC !== 'number' || isNaN(CC)){
-window.alert("Its gotta be a number silly!");
+  window.alert("heeyyyy, theres no data here!");
+
+ if (typeof CC !== 'number'|| isNaN(CC) === true){
+window.alert("And its gotta be a number");
+document.getElementById("KK").textContent = ("I willl be selling thiss!!")
 }
 
 
